6b155015d9b28f0fcdcf4b627afd6dc082f96c01
Ivan Solomichev <vanijatko.social@gmail.com>
2019-10-16 17:38:13 +0300

    renamed

--- contracts/SARToken.sol
+++ contracts/SARToken.sol
@@ -1,7 +1,7 @@
 pragma solidity >=0.4.21 <0.6.0;
 
-import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
-import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 
 
 contract sar_token is ERC20, ERC20Detailed("Saudi Arabia RE Token", "SAR", 18) {
