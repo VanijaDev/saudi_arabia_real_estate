827e3b1af40748056e12d18c30f73950c4d04d18
Ivan Solomichev <vanijatko.social@gmail.com>
2019-10-17 14:19:23 +0300

    Added description

--- contracts/SARToken.sol
+++ contracts/SARToken.sol
@@ -3,7 +3,9 @@ pragma solidity >=0.4.21 <0.6.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 
-
+/**
+  @dev Token to used for realty trading as payment method.
+ */
 contract sar_token is ERC20, ERC20Detailed("Saudi Arabia RE Token", "SAR", 18) {
   constructor() public {
     _mint(msg.sender, 1000);
